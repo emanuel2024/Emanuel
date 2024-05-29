d9ad97015022bac92834496d4f215ed0a76c49d4
emanuel2024 <161736554+emanuel2024@users.noreply.github.com>
2024-05-29 09:43:33 -0300

    kpvvkrovjvjrivjr

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("9034-05-24T00:00:00");
-const tempoObjetivo2 = new Date("7080-05-24T00:00:00");
-const tempoObjetivo3 = new Date("3050-05-24T00:00:00");
-const tempoObjetivo4 = new Date("3050-05-24T00:00:00");
+const tempoObjetivo1 = new Date("2090-05-24T00:00:00");
+const tempoObjetivo2 = new Date("2090-05-24T00:00:00");
+const tempoObjetivo3 = new Date("2090-05-24T00:00:00");
+const tempoObjetivo4 = new Date("2090-05-24T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
